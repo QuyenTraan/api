e105d8e6de2f516b76b7533305eebc4268e7fc3d
QuyenTraan <=>
2019-08-25 21:46:49 +0700

    api co san

---
+++ API_Angular/server/src/controllers/index.ts
@@ -0,0 +1,6 @@
+
+export { default as TinTucController } from './tintuc-controller'
+
+export { default as MonAnController } from './monan-controler'
+
+export { default as AuthController } from './auth-controller'
